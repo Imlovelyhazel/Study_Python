e6a746e92c07ed669ee0c5d4fc56f3e3d37f4355
Imlovelyhazel <khju0605@gmail.com>
2023-04-04 23:40:18 +0900

    230404 - Python

--- Example01.py
+++ Example01.py
@@ -1,8 +1,6 @@
 print(6 // 4)
 print(round(3.1415926535, 2))
 print("hello" * 3)
-print("현주")
-print("건이")
 print("Hello" + "world")
 print("3"+"5")
 print("I'm excited to learn Python!")
@@ -17,12 +15,21 @@ print(float(3))
 print(int("2")+int("5"))
 #str(string):숫자형을 문자형으로 전환
 print(str(2) + str(5))
+# %는 나머지를 구하는 것
+print(4 % 2)
+# //는 버림 나눗셈(몫이 소수형으로 나왔을 때 소숫점 아래를 버림)
 
 age = 7
 print("제 나이는" + str(age) + "살입니다")
 #str을 쓰지 않았을 경우 문자형+숫자형+문자형이기 때문에 이어질 수 없음
 
-# %는 나머지를 구하는 것
-print(4 % 2)
+year = 2023
+month = 4
+day = 4
 
-# //는 버림 나눗셈(몫이 소수형으로 나왔을 때 소숫점 아래를 버림)
+#문자열 포맷팅 .format() : 형태를 잡아주는 것
+print("오늘은 {}년 {}월 {}일입니다.".format(year, month, day))
+
+date_string = "오늘은 {}년 {}월 {}일입니다."
+print(date_string.format(year, month, day))
+print(date_string.format(year, month, day+1))
