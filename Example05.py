ae667e678fda1c84f5c05125dbaedc229a470a0b
Imlovelyhazel <khju0605@gmail.com>
2023-04-18 00:29:43 +0900

    230418 - Python

--- Example05.py
+++ Example05.py
@@ -13,11 +13,14 @@ while i % 23 != 0:
     i += 1
 print (i)
 
-#if 함수 (만약: if, 그렇지 않으면: else)
-#if 조건 부분(불린 값):(들여쓰기 후) 수행 부분
+#if 함수 (만약: if, 그렇지 않으면: else / 불린 값이 true면 수행 부분이 실행, false면 else의 수행 부분이 실행)
+#if 조건 부분(불린 값):(들여쓰기 후) 수행 부분 / else문은 if문 아래에 불린값이 false인 경우 어떻게 할지 쓰는 것
 
 temperature = 16
 if temperature <= 10:
     print("자켓을 입는다.")
 else:
     print("자켓을 입지 않는다")
+    
+#elif 문: if문과 else문에서 경우의 수가 너무 많을 경우 else: if 점수가 80점 이상이다: B를 준다 >> elif 점수가 80점 이상이다: B를 준다(else 밑으로 엔터가 없음)
+ 
